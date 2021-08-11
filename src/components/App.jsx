3044f2cdb990f27cc6410fbbe7c444062b7690a3
ojaswi-454 <aggojaswi@gmail.com>
2021-08-11 17:26:45 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
+
+function App(){
+    return (
+        <div>
+            <Sidebar />
+            <Home />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Home() {
+    return (
+
+        <div class="home">
+           
+            <ul>
+                <li><button>Add Post</button></li>
+                <li><button>Manage Post</button></li>
+                <li><button>Add People</button></li>
+                <li><button>Manage People</button></li>
+                
+            </ul>
+
+        </div>
+
+    );
+}
+
+
+export default Home;
